bfb8432d43ff46726de51a86600babca74894910
Ted Waller <ted.waller.iii@gmail.com>
2015-04-18 17:37:08 -0400

    local storage round 2

--- roller.js
+++ roller.js
@@ -2,47 +2,6 @@
 
   var actors, movies, title, fullList, button, refresh;
 
-  // Set Cookie to handle app refresh
-  docCookies = {
-    getItem: function (sKey) {
-      if (!sKey) { return null; }
-      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-    },
-    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-      var sExpires = "";
-      if (vEnd) {
-        switch (vEnd.constructor) {
-          case Number:
-            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-            break;
-          case String:
-            sExpires = "; expires=" + vEnd;
-            break;
-          case Date:
-            sExpires = "; expires=" + vEnd.toUTCString();
-            break;
-        }
-      }
-      document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-      return true;
-    },
-    removeItem: function (sKey, sPath, sDomain) {
-      if (!this.hasItem(sKey)) { return false; }
-      document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-      return true;
-    },
-    hasItem: function (sKey) {
-      if (!sKey) { return false; }
-      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-    },
-    keys: function () {
-      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-      for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
-      return aKeys;
-    }
-  };
-
 
   actors= [
     // 'Angelina Jolie',
@@ -64,7 +23,6 @@
     // 'Robin Williams',
     // 'Tom Cruise',
     // 'Tom Hanks',
-    // 'Tommy Lee Jones',
     // 'Tommy Lee Jones',
     'Will Smith'
   ],
@@ -106,11 +64,11 @@
 
   function isListEmpty () {
 
-    if (docCookies.getItem('options') === null) {
+    if (window.localStorage.getItem('options') === "") {
       return true;
     }
 
-    var list = docCookies.getItem('options').split(',');
+    var list = window.localStorage.getItem('options').split(',');
 
     if (list.length < 1) {
       return true;
@@ -121,10 +79,10 @@
 
 
   function roll () {
-    if (!docCookies.hasItem('options') || isListEmpty()) {
+    if (!window.localStorage.getItem('options') || isListEmpty()) {
       makeList();
     } else if (fullList === undefined) {
-      fullList = docCookies.getItem('options').split(',');
+      fullList = window.localStorage.getItem('options').split(',');
     }
 
     var randomIndex = Math.floor(Math.random() * ((fullList.length - 1) - 0));
@@ -133,7 +91,7 @@
 
     console.log(title.innerHTML);
 
-    docCookies.setItem('options', fullList);
+    window.localStorage.setItem('options', fullList);
 
     return;
   }
