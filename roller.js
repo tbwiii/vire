b33fb29f838a18b2ecbc15ea1f83e6bb53e13f49
Ted Waller <ted.waller.iii@gmail.com>
2015-04-18 16:44:34 -0400

    removed link to appcache to try sessions

--- roller.js
+++ roller.js
@@ -100,5 +100,12 @@
 
   reloadButton.onclick = refresh;
 
+  // Start or resume session
+  session_start(); 
+
+  // Extend cookie life time by an amount of your liking
+  $cookieLifetime = 365 * 24 * 60 * 60; // A year in seconds
+  setcookie(session_name(),session_id(),time()+$cookieLifetime);
+
 
 })(window)
