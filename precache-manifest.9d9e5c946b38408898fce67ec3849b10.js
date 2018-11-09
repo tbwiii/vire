fce75daa665cee330809880c77141d0414aa25fa
Ted Waller <ted.waller.iii@gmail.com>
2018-11-09 00:39:22 -0500

    Updates

---
+++ precache-manifest.9d9e5c946b38408898fce67ec3849b10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a12d569217199b2d96ac",
+    "url": "/vire/static/js/runtime~main.a12d5692.js"
+  },
+  {
+    "revision": "58598fc5b1ee72ff908b",
+    "url": "/vire/static/js/main.58598fc5.chunk.js"
+  },
+  {
+    "revision": "4d6953bae71cc0bed0c5",
+    "url": "/vire/static/js/1.4d6953ba.chunk.js"
+  },
+  {
+    "revision": "58598fc5b1ee72ff908b",
+    "url": "/vire/static/css/main.779f8bd4.chunk.css"
+  },
+  {
+    "revision": "d7bcf8262a6a6e4ef87d9b1eabe9617b",
+    "url": "/vire/index.html"
+  }
+];
